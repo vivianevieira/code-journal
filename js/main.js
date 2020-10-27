4050879eaef450184c2f4cfe2748858174327ca0
Viviane Vieira <viviane.kodama@gmail.com>
2020-10-26 17:12:47 -0700

    Listen for submit events on the form.

--- js/main.js
+++ js/main.js
@@ -2,5 +2,8 @@ var urlInput = document.querySelector('input[name="avatarUrl"]');
 
 urlInput.addEventListener('input', function (event) {
   document.getElementById('avatar').src = event.target.value;
-
 });
+
+var form = document.querySelector('form');
+
+form.addEventListener('submit');
