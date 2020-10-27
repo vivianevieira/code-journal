20d7a49a25bb2cfe6d1acbdf4ceaffc57b59a724
Viviane Vieira <viviane.kodama@gmail.com>
2020-10-26 20:26:02 -0700

    Reset the form inputs when the form is submitted. #1

--- js/main.js
+++ js/main.js
@@ -4,6 +4,14 @@ urlInput.addEventListener('input', function (event) {
   document.getElementById('avatar').src = event.target.value;
 });
 
-var form = document.querySelector('form');
+var $form = document.querySelector('.profile');
 
-form.addEventListener('submit');
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  data.profile.username = $form.elements.username.value;
+  data.profile.fullName = $form.elements.fullName.value;
+  data.profile.location = $form.elements.location.value;
+  data.profile.avatarUrl = $form.elements.avatarUrl.value;
+  data.profile.bio = $form.elements.bio.value;
+  $form.reset();
+});
