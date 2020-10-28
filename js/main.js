322149c92cb1dbc7ebca8609d04224e1846a2b76
Viviane Vieira <viviane.kodama@gmail.com>
2020-10-27 17:47:17 -0700

    Define a JavaScript function that takes the profile data model and returns a DOM tree that matches the HTML.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 var $urlInput = document.querySelector('input[name="avatarUrl"]');
-var $avatar = document.getElementById('avatar');
+var $avatar = document.querySelector('.avatar');
 
 $urlInput.addEventListener('input', function (event) {
   $avatar.src = event.target.value;
@@ -22,3 +22,64 @@ window.addEventListener('beforeunload', function (event) {
   var dataJson = JSON.stringify(data);
   localStorage.setItem('data-profile-storage', dataJson);
 });
+
+function domTree() {
+  var $profileDataView = document.querySelector("profile");
+
+  var $divContainer = document.createElement("div");
+  $divContainer.setAttribute("class", "container");
+  $profileDataView.appendChild($divContainer);
+
+  var $fullName = document.createElement("h2");
+  $fullName = data.profile.fullName;
+  $divContainer.appendChild($fullName);
+
+  var $divRow = document.createElement("div");
+  $divRow.setAttribute("class", "row");
+  $divContainer.appendChild($divRow);
+
+  var $divAvatar = document.createElement("div");
+  $divAvatar.setAttribute("class", "column-half");
+  $divRow.appendChild($divAvatar);
+
+  var $imgAvatar = document.createElement("img");
+  $imgAvatar.src = data.profile.avatarUrl;
+  $divAvatar.appendChild($imgAvatar);
+
+  var $divRightColumn = document.createElement("div");
+  $divRightColumn.setAttribute("class", "column-half");
+  $divRow.appendChild($divRightColumn);
+
+  var $usernameParagraph = document.createElement("p");
+  $divRightColumn.appendChild($usernameParagraph);
+
+  var $iconUsername = document.createElement("img");
+  $iconUsername.setAttribute("alt", "username-icon");
+  $iconUsername.setAttribute("class", "icon");
+  $usernameParagraph.appendChild($iconUsername);
+
+  var $spanUsername = document.createElement("span");
+  $spanUsername.setAttribute("id", "userId");
+  $spanUsername.textContent = data.profile.username;
+  $usernameParagraph.appendChild($spanUsername);
+
+  var $locationParagraph = document.createElement("p");
+  $divRightColumn.appendChild($locationParagraph);
+
+  var $iconLocation = document.createElement("img");
+  $iconLocation.setAttribute("alt", "location-icon");
+  $iconLocation.setAttribute("class", "icon");
+  $locationParagraph.appendChild($iconLocation);
+
+  var $spanLocation = document.createElement("span");
+  $spanLocation.setAttribute("id", "locationId");
+  $spanLocation.textContent = data.profile.location;
+  $locationParagraph.appendChild($spanLocation);
+
+  var $bioParagraph = document.createElement("p");
+  $bioParagraph.setAttribute("id", "bioId");
+  $bioParagraph.textContent = data.profile.bio;
+  $divRightColumn.appendChild($bioParagraph);
+
+  return $divContainer;
+}
